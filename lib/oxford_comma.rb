5f5af27cb8876472d81d1bfa174e97c207eb9edb
James Bridges <masterbridges@gmail.com>
2019-12-18 22:28:10 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,11 @@ def oxford_comma(array)
     return array.join(" and ")
   elsif array.size == 1
     return array.join
-  elsif array.size <= 3
+  elsif array.size = 3
+    newarray = array.pop
+    array = array.join(", ")
+    return "#{array}, and #{newarray}" 
+  elsif array.size < 3
     newarray = array.pop
     array = array.join(", ")
     return "#{array}, and #{newarray}" 
